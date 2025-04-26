45f39856ffaa9cd464b2a4c1963b2488c0d97f18
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2025-04-27 00:34:59 +0700

    tambah staticfile

--- core/settings.py
+++ core/settings.py
@@ -157,6 +157,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if not DEBUG:
