ba451cc91e3b175a32ae3fb8b0498bc4dcb1503e
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2025-06-10 20:13:02 +0700

    sudah bisa mengakses excel dan menampilkannya

--- dokumen/urls.py
+++ dokumen/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('search/', views.search_view, name='search'),
+    path('excel/', views.excel_table_view, name='excel_table'),
 ]
--- dokumen/views.py
+++ dokumen/views.py
@@ -1,5 +1,9 @@
+import pandas as pd
+import django_tables2 as tables
 from django.shortcuts import render
 from .search import DocumentDocument
+from django.shortcuts import render
+from django_tables2 import RequestConfig
 
 def search_view(request):
     query = request.GET.get('q', '')
@@ -15,3 +19,23 @@ def search_view(request):
         results = response.hits
 
     return render(request, 'dokumen/search_results.html', {'results': results, 'query': query})
+
+
+
+
+def excel_table_view(request):
+    df = pd.read_excel('D:/Cloud/2025/REALISASI 2025/Realisasi/06 Juni.xlsx')
+
+    # Buat table class dinamis dari kolom-kolom di DataFrame
+    class ExcelTable(tables.Table):
+        class Meta:
+            attrs = {"class": "table table-bordered table-striped"}
+
+    for col in df.columns:
+        ExcelTable.base_columns[col] = tables.Column(verbose_name=col)
+
+    table = ExcelTable(df.to_dict(orient='records'))
+
+    return render(request, 'dokumen/excel_table.html', {
+        'table': table
+    })
